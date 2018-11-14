50f20a380ed6dfcb3ddf6704a6fb85fff153846f
JNelson <nelson.jimmy@gmail.com>
2018-11-14 10:44:15 -0700

    Updates if statement formatting.

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -4,12 +4,9 @@ class RaindropConverter {
         if (number % 3 != 0 && number % 5 != 0 && number % 7 != 0)
             return String.valueOf(number);
         String message = "";
-        if (number % 3 == 0)
-            message = message + "Pling";
-        if (number % 5 == 0)
-            message = message +  "Plang";
-        if (number % 7 == 0)
-            message = message +  "Plong";
+        if (number % 3 == 0) message += "Pling";
+        if (number % 5 == 0) message += "Plang";
+        if (number % 7 == 0) message += "Plong";
         return message;
     }
 
